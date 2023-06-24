c38011538be8c152573fd8d86f2ec34df9f87551
luowenjun@inke.cn <luowenjun@inke.cn>
2023-06-25 02:21:57 +0800

    feat: aliyun

--- aliyun/security_group/main.go
+++ aliyun/security_group/main.go
@@ -1,5 +1,80 @@
+// This file is auto-generated, don't edit it. Thanks.
 package main
 
-func main() {
+import (
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	ecs20140526 "github.com/alibabacloud-go/ecs-20140526/v3/client"
+	util "github.com/alibabacloud-go/tea-utils/v2/service"
+	"github.com/alibabacloud-go/tea/tea"
+	"os"
+)
 
+/**
+ * 使用AK&SK初始化账号Client
+ * @param accessKeyId
+ * @param accessKeySecret
+ * @return Client
+ * @throws Exception
+ */
+func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *ecs20140526.Client, _err error) {
+	config := &openapi.Config{
+		// 必填，您的 AccessKey ID
+		AccessKeyId: accessKeyId,
+		// 必填，您的 AccessKey Secret
+		AccessKeySecret: accessKeySecret,
+	}
+	// 访问的域名
+	config.Endpoint = tea.String("ecs-cn-hangzhou.aliyuncs.com")
+	_result = &ecs20140526.Client{}
+	_result, _err = ecs20140526.NewClient(config)
+	return _result, _err
+}
+
+func _main(args []*string) (_err error) {
+	// 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+	// 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378661.html
+	client, _err := CreateClient(tea.String(os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_ID")), tea.String(os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")))
+	if _err != nil {
+		return _err
+	}
+
+	modifySecurityGroupRuleRequest := &ecs20140526.ModifySecurityGroupRuleRequest{
+		RegionId: tea.String("your_value"),
+	}
+	tryErr := func() (_e error) {
+		defer func() {
+			if r := tea.Recover(recover()); r != nil {
+				_e = r
+			}
+		}()
+		// 复制代码运行请自行打印 API 的返回值
+		_, _err = client.ModifySecurityGroupRuleWithOptions(modifySecurityGroupRuleRequest, &util.RuntimeOptions{})
+		if _err != nil {
+			return _err
+		}
+
+		return nil
+	}()
+
+	if tryErr != nil {
+		var error = &tea.SDKError{}
+		if _t, ok := tryErr.(*tea.SDKError); ok {
+			error = _t
+		} else {
+			error.Message = tea.String(tryErr.Error())
+		}
+		// 如有需要，请打印 error
+		_, _err = util.AssertAsString(error.Message)
+		if _err != nil {
+			return _err
+		}
+	}
+	return _err
+}
+
+func main() {
+	err := _main(tea.StringSlice(os.Args[1:]))
+	if err != nil {
+		panic(err)
+	}
 }
