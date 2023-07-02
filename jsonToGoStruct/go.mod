25a47f40dc17a3a84b8ebc18f48d36318213a848
luowenjun@inke.cn <luowenjun@inke.cn>
2023-07-03 01:23:19 +0800

    feat: json tool

--- jsonToGoStruct/go.mod
+++ jsonToGoStruct/go.mod
@@ -2,7 +2,9 @@ module gotools_jsonToGoStruct
 
 go 1.18
 
+require github.com/ChimeraCoder/gojson v1.1.0
+
 require (
-	github.com/ChimeraCoder/gojson v1.1.0 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- jsonToGoStruct/main.go
+++ jsonToGoStruct/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/ChimeraCoder/gojson"
+	"github.com/atotto/clipboard"
+	"os"
 	"strings"
 )
 
@@ -11,9 +14,12 @@ func main() {
 	inputJson := ""
 	structName := ""
 	pkgName := ""
+	inputTags := "json"
 	flag.StringVar(&inputJson, "j", "", "json body input")
 	flag.StringVar(&structName, "n", "Data", "generate struct name")
 	flag.StringVar(&pkgName, "p", "main", "generate struct package name")
+	flag.StringVar(&inputTags, "t", "json", "generate struct inputTags")
+	useSub := flag.Bool("sub", false, "generate sub struct")
 	flag.Parse()
 
 	if inputJson == "" {
@@ -21,11 +27,42 @@ func main() {
 		return
 	}
 
-	generate, err := gojson.Generate(strings.NewReader(inputJson), gojson.ParseJson, structName, pkgName, []string{"json"}, false, true)
+	tags := strings.Split(inputTags, ",")
+
+	// distinct tag
+	tagMap := make(map[string]struct{})
+	for _, tag := range tags {
+		tagMap[tag] = struct{}{}
+	}
+	tagMap["json"] = struct{}{}
+
+	tags = tags[:0]
+	for tag, _ := range tagMap {
+		tags = append(tags, tag)
+	}
+
+	generate, err := gojson.Generate(strings.NewReader(inputJson), gojson.ParseJson, structName, pkgName, tags, *useSub, true)
 	if err != nil {
 		fmt.Printf("generate err:%s\n", err)
 		return
 	}
 
-	fmt.Println(generate)
+	fmt.Printf("output struct:\n\n")
+	fmt.Println(string(generate))
+	fmt.Println()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Need to copy the output to the clipboard?(y/other)")
+	scanner.Scan()
+	text := scanner.Text()
+	if strings.ToLower(text) == "y" {
+		err := clipboard.WriteAll(string(generate))
+		if err != nil {
+			fmt.Printf("copy to clipboard failed, err:%s", err)
+			return
+		}
+	} else {
+
+	}
+	fmt.Println("copy to clipboard success")
 }
